5ce0b60971dc02540d29559d1d917150eac10012
YeonV <trusted.yeon@gmail.com>
2025-06-10 22:19:38 +0200

    remove styles and add textfield

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,22 @@
 "use client";
 import Image from "next/image";
 import styles from "./page.module.css";
-import { Button } from "@mui/material";
+import { Button, TextField } from "@mui/material";
 import { LocalAirport } from "@mui/icons-material";
 import TopBar from "@/components/TopBar";
 import Details from "@/components/Details";
 import SummaryCard from "@/components/SummaryCard";
 import Gallery from "@/components/Gallery";
+import { useState } from "react";
 
 export default function Home() {
+  const [userInput, setUserInput] = useState("");
   return (
     <>
       <TopBar />
       <div className={styles.page}>
         <main className={styles.main}>
+          <TextField id="standard-basic" label="Standard" variant="standard" value={userInput} onChange={(e) => setUserInput(e.target.value)}/>
           <Button variant="outlined" startIcon={<LocalAirport />}>
             Croatia Trip 2025
           </Button>
