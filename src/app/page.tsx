e6af779353932c3ae02e2ed0e6642df89a237165
THz <tzheusser@gmail.com>
2025-06-10 14:04:45 +0200

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,81 @@
+"use client"
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Avatar, Box, ImageList, ImageListItem, ImageListItemBar, Slider, Stack } from "@mui/material";
 
+const itemData = [
+  {
+    img: 'water.jpeg',
+    title: 'Rocky beach',
+    author: '@tristan_heusser',
+  },
+  {
+    img: 'burger.jpeg',
+    title: 'Burger',
+    author: '@tristan_heusser',
+  },
+  {
+    img: 'pizza.jpeg',
+    title: 'Pizza',
+    author: '@tristan_heusser',
+  },
+  {
+    img: 'icecream.jpeg',
+    title: 'Icecream',
+    author: '@tristan_heusser',
+  },
+  {
+    img: 'friends.jpeg',
+    title: 'Friends',
+    author: '@tristan_heusser',
+  },
+  {
+    img: 'dinner.jpeg',
+    title: 'Dinner',
+    author: '@tristan_heusser',
+  },
+  {
+    img: 'evening.jpeg',
+    title: 'Evening',
+    author: '@tristan_heusser',
+  },
+  {
+    img: 'basketball.jpeg',
+    title: 'Basketball',
+    author: '@tristan_heusser',
+  },
+];
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
+          src="/landscape.jpeg"
+          alt="Ocean"
           priority
+          width={4032/3}
+          height={3024/3}
         />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+        
+  <Avatar alt="Remy Sharp" src="/avatar.jpeg" />
+    <ImageList sx={{ width: "100%", height: 900 }}>
+      {itemData.map((item) => (
+        <ImageListItem key={item.img}>
+          <img
+            srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+            src={`${item.img}?w=248&fit=crop&auto=format`}
+            alt={item.title}
+            loading="lazy"
           />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+          <ImageListItemBar
+            title={item.title}
+            subtitle={<span>by: {item.author}</span>}
+            position="below"
           />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </ImageListItem>
+      ))}
+    </ImageList>
+  </main>
     </div>
   );
 }
