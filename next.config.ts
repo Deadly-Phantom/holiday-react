e6909955375aa3dd34576cc75b4af11afc44814c
THz <tzheusser@gmail.com>
2025-06-11 20:11:15 +0200

    feat(config): add basePath and disable image optimization in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/holiday-react",
+  images: {
+    unoptimized: true, // Disable Next.js image optimization
+  }
 };
 
 export default nextConfig;
